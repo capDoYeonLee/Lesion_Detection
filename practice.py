21333574b852d11b6f7f22c4272d03ce54100a36
capDoYeonLee <ghtyu21@naver.com>
2022-01-10 00:06:17 +0900

    modi

--- practice.py
+++ practice.py
@@ -25,18 +25,18 @@ from collections import defaultdict, deque
 
 
 
-train_files = sorted(glob('./Datasets/train/*'))
-test_files = sorted(glob('./Datasets/test/*'))
+train_files = sorted(glob('../Datasets/train/*'))
+test_files = sorted(glob('../Datasets/test/*'))
 
 
 
 train_json_list = []
-for file in tqdm(train_files[:10]):
+for file in tqdm(train_files[:100]):
     with open(file, "r") as json_file:
         train_json_list.append(json.load(json_file))
 
 test_json_list = []
-for file in tqdm(test_files[:10]):
+for file in tqdm(test_files[:100]):
     with open(file, "r") as json_file:
         test_json_list.append(json.load(json_file))
 
@@ -52,7 +52,7 @@ for data in train_json_list:
 
 print(label_count)
 
-
+'''
 plt.figure(figsize=(25,30))
 for i in range(5):
     plt.subplot(1,5,i+1)
@@ -69,3 +69,82 @@ for i in range(5):
     plt.imshow(img)
     plt.subplot(1,5,i+1).set_title(title)
 plt.show()    
+'''
+
+
+
+class CustomDataset(Dataset):
+    def __init__(self, json_list, mode='train'):
+        self.mode = mode
+        self.file_name = [json_file['file_name'] for json_file in json_list]
+        if mode == 'train':
+            
+            self.labels = []
+            for data in json_list:
+                # label is lesion category  >  ex) 04_lymph...
+                label = []
+                for shapes in data['shapes']:
+                    label.append(shapes['label'])
+                self.labels.append(label)
+                
+                # points is coordinate     >  ex) (x1,y1)...
+            self.points = []
+            for data in json_list:
+                point = []
+                for shapes in data['shapes']:
+                    point.append(shapes['points'])
+                self.points.append(point)
+                
+        self.imgs = [data['imageData'] for data in json_list]    # decoded data
+        
+        self.widths = [data['imageWidth'] for data in json_list]
+        self.heights = [data['imageHeight'] for data in json_list]
+        
+        self.label_map ={
+            '01_ulcer':1, '02_mass':2, '04_lymph':3, '05_bleeding':4
+        }
+        
+        self.transforms = transforms.Compose([
+            transforms.ToTensor(),
+        ])
+        
+    def __len__(self):
+        return len(self.imgs)
+    
+    def __getitem__(self, i):
+        file_name = self.file_name[i]
+        img = Image.open(BytesIO(base64.b64decode(self.imgs[i])))
+        img = self.transforms(img)
+        
+        target = {}
+        if self.mode == 'train':
+            boxes = []
+            for point in self.points[i]:
+                x_min = int(np.min(np.array(point)[:,0]))
+                x_max = int(np.max(np.array(point)[:,0]))
+                y_min = int(np.min(np.array(point)[:,1]))
+                y_max = int(np.max(np.array(point)[:,1]))
+                boxes.append([x_min, y_min, x_max, y_max])
+            boxes = torch.as_tensor(boxes, dtype=torch.float32)
+
+            area = (boxes[:, 3] - boxes[:, 1]) * (boxes[:, 2] - boxes[:, 0])
+            iscrowd = torch.zeros((len(boxes),), dtype=torch.int64)
+
+            label = [self.label_map[label] for label in self.labels[i]]
+
+            masks = []
+            for box in boxes:
+                mask = np.zeros([int(self.heights[i]), int(self.widths[i])], np.uint8)
+                masks.append(cv2.rectangle(mask, (int(box[0]), int(box[1])), (int(box[2]), int(box[3])), 1, -1))
+
+            masks = torch.tensor(masks, dtype=torch.uint8)
+
+            target["boxes"] = boxes
+            target["labels"] = torch.tensor(label, dtype=torch.int64)
+            target["masks"] = masks
+            target["area"] = area
+            target["iscrowd"] = iscrowd
+        target["image_id"] = torch.tensor([i], dtype=torch.int64)
+        if self.mode == 'test':
+            target["file_name"] = file_name
+        return img, target
